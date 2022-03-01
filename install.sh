3af0d0fd086cbf9b1690116c257c3f0ed52532ed
mdgqdjqr <mdgqdjqr@163.com>
2022-03-01 15:34:53 +0800

    update files

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 npm install
 npm install hexo-deployer-git --save
+npm uninstall hexo-asset-image --save
+npm install https://github.com/CodeFalling/hexo-asset-image --save
+cp config/index.js node_modules/hexo-asset-image/index.js -a
