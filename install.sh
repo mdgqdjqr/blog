1a00590b321a6145c29d17cafb42d566cf590260
mdgqdjqr <mdgqdjqr@163.com>
2022-03-04 16:20:22 +0800

    更换MathJax公式渲染引擎

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ npm install hexo-deployer-git --save
 
 npm uninstall hexo-renderer-marked --save
 npm install hexo-renderer-kramed --save
-cp config/index.js node_modules/kramed/lib/rules/inline.js -a
+cp config/inline.js node_modules/kramed/lib/rules/inline.js -a
 
 npm uninstall hexo-asset-image --save
 npm install https://github.com/CodeFalling/hexo-asset-image --save
